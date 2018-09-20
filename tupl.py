e73c13811452b9a4d672902aac438b432b190961
root <sudhanshurr@gmail.com>
2018-09-20 11:41:18 +0530

    hellomotherfucker

---
+++ tupl.py
@@ -0,0 +1,19 @@
+
+#tuple1 = (0, 1, 2, 3)
+#tuple2 = ('python', 'geek')
+
+# Concatenating above two
+#print(tuple1 + tuple2)
+
+#vegetable = ["brinjal,gobi,aloo,"]
+#for x in vegetable:
+#    print(x)
+
+#hello =[hi,how]
+#print (hello [0][-1])
+
+tup = ('var','caa','aaa')
+n = 5  #Number of time loop runs
+for i in range(int(n)):
+    tup = (tup,)
+    print(tup)
