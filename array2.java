ed00fec92495578985f4efaeb6f84fd8a442c18f
root <sudhanshurr@gmail.com>
2018-09-20 12:00:10 +0530

    hellomotherfucker

---
+++ array2.java
@@ -0,0 +1,31 @@
+
+
+class array2
+{
+public static void main(String []args)
+{
+String [] hello= new String [5];
+
+hello[0]="india";
+hello[1]="cannada";
+hello[2]="aus";
+hello[3]="newzeland";
+hello[4]="usa";
+
+for(int i =0 ; i<5 ; i ++)
+{
+System.out.println("value at index "+ i + " is "+hello[i]);
+}
+
+
+//int inp;
+int[] val = new int[inp];//
+
+//for(iny i =0 ; i <inp ;i++)
+{//
+
+//val[i]=;
+}//
+
+}
+}
