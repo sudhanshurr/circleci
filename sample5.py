6258d00f4a7fc18222878c2d45c98b4dfe256be5
root <sudhanshurr@gmail.com>
2018-09-20 11:39:59 +0530

    hellomotherfucker

---
+++ sample5.py
@@ -0,0 +1,14 @@
+#for printing the character at given index
+var="techycardia"
+print var[6]
+#for printing the before charcaters
+print var[:6]
+#it will include 1 char till 5 character
+print var[1:5]
+#starting from right
+print var[6:]
+#
+print var[0:2:5]
+#
+print var[0:6:2]
+#
